96715f209a0a67c74ca2443fbb785cbd1813a9e9
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-07-18 19:45:02 -0400

    working on API

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
+// Core node
+const fs = require("fs");
+//Express
 const express = require("express");
-
 const app = express();
 const port = 3000;
 
+// MIDLEWARE
+app.use(express.json());
+
 // * ROUTS
-app.get("/", (res, req) => {
-    res.statusCode(200).send("Server is up and running...");
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (error) => {
+            res.status(201).json({
+                status: "succes",
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
 });
 
 app.listen(port, () => {
