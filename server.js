77c5fcdd28808a01fdb0939dcb87e2980b88cde6
Sergei <koshelevworkmail@gmail.com>
2022-07-20 21:00:27 -0400

    Connecting MongoDB/Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+/* eslint-disable prettier/prettier */
 // CONFIG
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB CONNECTED..."));
 // require("dotenv").config({ path: "./config.env" });
 
 const app = require("./app");
