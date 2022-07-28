bd18749efef084fe2c306d837647c2faf9ec2059
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-07-28 16:09:16 -0400

    Password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -28,7 +29,23 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, "Confirm your password."],
+        validate: {
+            // ! WORKS ONLY ON SAVE
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: "Passwords are not the same.",
+        },
     },
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model("User", userSchema);
