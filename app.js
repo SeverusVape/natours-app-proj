aa699471f0991addb88091c33a7f0d5d41f690c1
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-08-02 17:20:01 -0400

    express-mongo-sanitize && xss-clean middlewares

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const morgan = require("morgan");
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xssClean = require("xss-clean");
 //
 const AppError = require("./utils/appError");
 const globalError = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRouts");
 const userRouter = require("./routes/userRouts");
 
-// EXPRESS
+// * EXPRESS
 const app = express();
 
-// GLOBAL MIDLEWARE
+// * GLOBAL MIDLEWAREs
 // set security http headers
 app.use(helmet());
 
@@ -32,7 +34,10 @@ app.use("/api", limiter);
 
 // body parser (req.body)
 app.use(express.json({ limit: "10kb" }));
-
+// data sanitization noSQL
+app.use(mongoSanitize());
+//data sanitization XSS
+app.use(xssClean());
 // serving static
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
