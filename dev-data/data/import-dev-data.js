bbf5f20124fe4d5a7cc4b0c22da1e3708b59cc0d
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-08-05 18:42:00 -0400

    Denormalizing DB

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -21,9 +21,7 @@ mongoose
     .then(() => console.log("DB CONNECTED..."));
 
 // Reading
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
 
 // Importing to DB
 const importData = async () => {
