6c640358610215879f5e95db5c236ef505e35b53
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-08-07 19:56:02 -0400

    Average Rating calculation on Tours

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const Tour = require("../../models/tourModel");
+const User = require("../../models/userModel");
+const Review = require("../../models/reviewModel");
 
 dotenv.config({ path: "../../config.env" });
 
@@ -22,11 +24,17 @@ mongoose
 
 // Reading
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 // Importing to DB
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log("DATA loaded successfully.");
     } catch (err) {
         console.error(err);
@@ -38,6 +46,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log("All DATA in DB deleted.");
     } catch (err) {
         console.error(err);
