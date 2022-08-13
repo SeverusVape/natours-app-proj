1b647f495d0ff994606b3f8b1f0a4f439a5520f0
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-08-13 19:03:05 -0400

    map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,10 +1,8 @@
 const locations = JSON.parse(document.getElementById("map").dataset.locations);
 
 mapboxgl.accessToken =
-    "pk.eyJ1Ijoic2V2ZXJ1c3ZhcGUiLCJhIjoiY2w2c2RybnVmMWQ4eDNqdGVuaTR4em5yZiJ9.2XCGOk4iNY_gUFQ90wiO2g";
+    "pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A";
 const map = new mapboxgl.Map({
     container: "map", // container ID
     style: "mapbox://styles/mapbox/streets-v11", // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
 });
