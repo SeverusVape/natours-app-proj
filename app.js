2421cd9e1349a8de5c95d503c1cb1ecade099087
Veter v Golove <koshelevworkmail@gmail.com>
2022-08-17 20:28:01 -0400

    Updating user data

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use("/api", limiter);
 
 // body parser (req.body)
 app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 // data sanitization noSQL
 app.use(mongoSanitize());
