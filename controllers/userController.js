3b095a5ee8073af69d67e43bfa1949be9feb7423
Veter v Golove <koshelevworkmail@gmail.com>
2022-08-18 15:53:50 -0400

    crop image with sharp package

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,22 @@
 /* eslint-disable prettier/prettier */
 const User = require("../models/userModel");
 const multer = require("multer");
+const sharp = require("sharp");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const factory = require("./handlerFactory");
 
-const multerStorage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, "public/img/users");
-    },
-    filename: (req, file, callback) => {
-        const ext = file.mimetype.split("/")[1];
-        callback(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-    },
-});
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, "public/img/users");
+//     },
+//     filename: (req, file, callback) => {
+//         const ext = file.mimetype.split("/")[1];
+//         callback(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     },
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, callback) => {
     if (file.mimetype.startsWith("image")) {
@@ -29,6 +32,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = (req, res, next) => {
+    if (!req.file) return next();
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/users/${req.file.filename}`);
+
+    next();
+};
 
 const filterObj = (obj, ...alowedFields) => {
     const newObj = {};
@@ -63,6 +80,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     }
     // Update User document
     const filteredBody = filterObj(req.body, "name", "email");
+
+    if (req.file) filteredBody.photo = req.file.filename;
+
     const updatedUser = await User.findByIdAndUpdate(
         req.user.id,
         filteredBody,
